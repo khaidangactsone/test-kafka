773ad58a72f01c3bd597c46a34bd306d18e423be
khaidang <khaidang.actsone@gmail.com>
2024-04-10 18:19:10 +0700

    a

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -47,7 +49,6 @@ func ReadConfig(configFile string) kafka.ConfigMap {
 }
 
 func pruducer(quantity int) {
-
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <config-file-path>\n",
 			os.Args[0])
@@ -93,7 +94,6 @@ func pruducer(quantity int) {
 	// Wait for all messages to be delivered
 	p.Flush(100)
 	p.Close()
-
 }
 
 func handler(c *gin.Context) {
@@ -112,11 +112,60 @@ func handler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": elapsedTime})
 }
 
+func consumerHandler(con *gin.Context) {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <config-file-path>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	configFile := os.Args[1]
+	conf := ReadConfig(configFile)
+	conf["group.id"] = "test-group-comsumer"
+	conf["auto.offset.reset"] = "earliest"
+
+	c, err := kafka.NewConsumer(&conf)
+
+	if err != nil {
+		fmt.Printf("Failed to create consumer: %s", err)
+		os.Exit(1)
+	}
+
+	topic := "purchases"
+	err = c.SubscribeTopics([]string{topic}, nil)
+	// Set up a channel for handling Ctrl-C, etc
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Process messages
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev, err := c.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				// Errors are informational and automatically handled by the consumer
+				continue
+			}
+			fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
+				*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+		}
+	}
+
+	c.Close()
+	con.JSON(http.StatusOK, gin.H{"message": "Consumer is closed"})
+}
+
 func main() {
 	router := gin.Default()
 
 	// Define a GET request handler at '/'
 	router.GET("/", handler)
+
+	router.GET("/consumer", consumerHandler)
 
 	// Start the server on port 8080
 	router.Run(":8089")
